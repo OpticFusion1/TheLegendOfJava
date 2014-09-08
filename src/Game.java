aaf705d359c104aeb232e47454a207eab9a800e1
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2014-09-09 00:39:22 +0200

    Updated Main

--- src/Game.java
+++ src/Game.java
@@ -9,22 +9,28 @@ public class Game {
 		Scanner input = new Scanner (System.in);
 		
 		// Player name
-		System.out.println("Welcome to Legend of Java /n Please enter your name:");
+		System.out.println("Welcome to Legend of Java \n Please enter your name:");
 		String playerName = input.next();
 		
 		// Player difficulty and health
-		System.out.println("Do you wish to play the game on Easy, Medium or Hard? /n Type E, M or H");
+		System.out.println("Do you wish to play the game on Easy, Medium or Hard? \n Type E, M or H");
 		String difficultyLevel = input.next();
 		int playerhealth = 0;
-		if (difficultyLevel == "E") {
-			playerhealth = 100; 
-		} else if (difficultyLevel == "M") {
-			playerhealth = 80;
-		} else if (difficultyLevel == "H") {
-			playerhealth = 60;
-		} else {
+		while (!(difficultyLevel.equalsIgnoreCase("E") || difficultyLevel.equalsIgnoreCase("M") || difficultyLevel.equalsIgnoreCase("H"))) {
 			System.out.println("You didn't enter the right character, type E, M or H");
+			difficultyLevel = input.next();
 		}
+			if (difficultyLevel.equalsIgnoreCase("E")) {
+				playerhealth = 100;
+				System.out.println("You choose to play the game on easy mode");
+			} else if (difficultyLevel.equalsIgnoreCase("M")) {
+				playerhealth = 80;
+				System.out.println("You choose to play the game on medium mode");
+			} else if (difficultyLevel.equalsIgnoreCase("H")) {
+				playerhealth = 60;
+				System.out.println("You choose to play the game on hard mode");
+			}
+		
 	}
 
 }
