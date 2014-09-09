a0fecd926f5a56d54ca267c2d6f4cdbd75fbf9d3
Niklas Stokkebro Gundlev <niklasgundlev@gmail.com>
2014-09-09 11:20:14 +0200

    small tweek

--- src/Game.java
+++ src/Game.java
@@ -9,17 +9,17 @@ public class Game {
 		Scanner input = new Scanner (System.in);
 		
 		// Player name
-		System.out.println("Welcome to Legend of Java \n Please enter your name:");
+		System.out.println("Welcome to Legend of Java \nPlease enter your name:");
 		String playerName = input.next();
 		
-		// Player difficulty and health
-		System.out.println("Do you wish to play the game on Easy, Medium or Hard? \n Type E, M or H");
+		// Player difficulty ---> health
+		System.out.println("\nDo you wish to play the game on Easy, Medium or Hard? \nType E, M or H");
 		String difficultyLevel = input.next();
 		byte playerHealth = 0;
 		int playerMaxDamage = 0;
 		int playerMinDamage = 0;
 		while (!(difficultyLevel.equalsIgnoreCase("E") || difficultyLevel.equalsIgnoreCase("M") || difficultyLevel.equalsIgnoreCase("H"))) {
-			System.out.println("You didn't enter the right character, type E, M or H");
+			System.out.println("\nYou didn't enter the right character, type E, M or H");
 			difficultyLevel = input.next();
 		}
 			if (difficultyLevel.equalsIgnoreCase("E")) {
@@ -41,8 +41,8 @@ public class Game {
 		
 		//Constructing player
 			Player Player1 = new Player(playerName, playerHealth, playerMaxDamage, playerMinDamage);
-			System.out.println("Hello " + playerName + ", \nYou start you quest with a full health of " + playerHealth + "\nAnd an attack interval of " + playerMinDamage + " - " + playerMaxDamage);
-			
+			System.out.println("\nHello " + playerName + "! \nYou start you quest with a full health of " + playerHealth + "\nand an attack interval of " + playerMinDamage + " - " + playerMaxDamage + ".");
+			System.out.println("Good luck against the monsters. \nLet the legend of Java BEGIN!");
 	}
 
 }
